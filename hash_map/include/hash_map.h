3e8ae335c43b4366493e592acf0e103bf6f9b962
amir_hs <ali01h72@gmail.com>
2026-04-28 15:11:59 +0300

    header update

--- hash_map/include/hash_map.h
+++ hash_map/include/hash_map.h
@@ -42,21 +42,21 @@ enum class hash_err_t
 
 //--------------------------------------------------------------------------------
 
-hash_err_t HashDump (hash_ctx_t* hash_ctx, const char* file_name);
-hash_err_t InitHashMap (hash_ctx_t** hash_ctx);
-int CountHashFunction (char* str, int str_len);
-hash_err_t HashInsert (hash_ctx_t* hash_ctx, char* str, int str_len);
-hash_err_t TakeHashData (hash_ctx_t* hash_ctx, char* file_name);
-hash_err_t GetFileName  (int argc, char** argv, char** file_name);
-bool HashFindElement (hash_ctx_t* hash_ctx, char* str, int str_len);
-hash_err_t HashInitNewStr (int str_len, char** buf);
-void HashDestroy (hash_ctx_t* hash_ctx);
-hash_err_t HashDumpForData (hash_ctx_t* hash_ctx, const char* file_name);
-hash_err_t InitBuffer (char** src,int size);
-hash_err_t ReadSrc    (const char* file_name, int file_size, char* src_buffer);
-int CountNumbers (int val);
-void ParseHashData (hash_ctx_t* hash_ctx, char* buffer, int file_name);
-int CopyCurStr (char* src, char* dst, int* pos, int file_size);
-hash_err_t HashTest (hash_ctx_t* hash_ctx, char* file_name);
+hash_err_t InitHashMap       (hash_ctx_t** hash_ctx                                 );
+hash_err_t InitBuffer        (char** src , int    size                              );
+hash_err_t HashInitNewStr    (int str_len, char** buf                               );
+hash_err_t TakeHashData      (hash_ctx_t* hash_ctx, char* file_name                 );
+hash_err_t HashTest          (hash_ctx_t* hash_ctx, char* file_name                 );
+hash_err_t GetFileName       (int argc, char** argv, char** file_name               );
+hash_err_t HashDumpForData   (hash_ctx_t* hash_ctx, const char* file_name           );
+hash_err_t HashDump          (hash_ctx_t* hash_ctx, const char* file_name           );
+hash_err_t HashInsert        (hash_ctx_t* hash_ctx, char* str, int str_len          );
+hash_err_t ReadSrc           (const char* file_name, int file_size, char* src_buffer);
+bool       HashFindElement   (hash_ctx_t* hash_ctx , char* str   , int str_len      );
+void       ParseHashData     (hash_ctx_t* hash_ctx , char* buffer, int file_name    );
+void       HashDestroy       (hash_ctx_t* hash_ctx                                  );
+int        CountNumbers      (int val                                               );
+int        CountHashFunction (char* str, int str_len                                );
+int        CopyCurStr        (char* src, char* dst, int* pos, int file_size         );
 
 //--------------------------------------------------------------------------------
